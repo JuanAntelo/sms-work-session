54d10cec6ab6c381f16bc25a93f460f9dc16bbc6
Juan Antelo <antelo.juan@gmail.com>
2018-02-22 12:15:47 -0500

    clean up padding

--- public/app.js
+++ public/app.js
@@ -1,37 +1,34 @@
-      angular.module('todoApp', []).controller('TodoListController', function($scope, $http) {
-        $scope.result = 'value not yet calculated';
+angular.module('todoApp', []).controller('TodoListController', function($scope, $http) {
+  $scope.result = 'value not yet calculated';
 
-        $scope.trigger = function() {
-          $scope.result = 'calculating..';
-          
-          setTimeout(function(){
-            $scope.result = 33;            
-          }, 1000);
-        }
+  $scope.trigger = function() {
+    $scope.result = 'calculating..';
+    setTimeout(function(){
+      $scope.result = 33;            
+    }, 1000);
+  }
 
+  const endpointA = '/calcA';
+  const endpointB = '/calcB';
 
-        var endpointA = '/calcA';
-        var endpointB = '/calcB';
+  $http.get(endpointA).then(function(response){
+    processEndpoint1(response.data);
+  },
+    processEndpointError
+  );
 
-        $http.get(endpointA).then(function(response){
-          processEndpoint1(response.data);
-        },
-          processEndpointError
-        );
+  function processEndpoint1(endpoint1Data) {
+    console.log('processEndpoint1 : ' + endpoint1Data);
+    $http.get(endpointB).then(function(endpoint2Data){
+      processEndpoint2(endpoint2Data.data)
+    }, processEndpointError);
+  }
 
-        function processEndpoint1(endpoint1Data) {
-          console.log('processEndpoint1 : ' + endpoint1Data);
+  function processEndpoint2(data) {
+    console.log('processEndpoint2 : ' + data);
+  }
 
-          $http.get(endpointB).then(function(endpoint2Data){
-            processEndpoint2(endpoint2Data.data)
-          }, processEndpointError);
-        }
-
-        function processEndpoint2(data) {
-          console.log('processEndpoint2 : ' + data);
-        }
-
-        function processEndpointError(response) {
-          console.log('error with an api request');
-        }
-      });
+  function processEndpointError(response) {
+    console.log('error with an api request');
+  }
+});
