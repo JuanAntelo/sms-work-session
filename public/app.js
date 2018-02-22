9a1c515fcab2063f8e3d2dcf11441c5e4c722d59
Juan Antelo <antelo.juan@gmail.com>
2018-02-22 12:13:53 -0500

    add some styles, remove unused code

--- public/app.js
+++ public/app.js
@@ -1,18 +1,29 @@
-      angular.module('todoApp', []).controller('TodoListController', function($http) {
+      angular.module('todoApp', []).controller('TodoListController', function($scope, $http) {
+        $scope.result = 'value not yet calculated';
+
+        $scope.trigger = function() {
+          $scope.result = 'calculating..';
+          
+          setTimeout(function(){
+            $scope.result = 33;            
+          }, 1000);
+        }
+
+
         var endpointA = '/calcA';
         var endpointB = '/calcB';
 
         $http.get(endpointA).then(function(response){
           processEndpoint1(response.data);
-        }, 
+        },
           processEndpointError
         );
 
-        function processEndpoint1(data) {
-          console.log('processEndpoint1 : ' + data);
+        function processEndpoint1(endpoint1Data) {
+          console.log('processEndpoint1 : ' + endpoint1Data);
 
-          $http.get(endpointB).then(function(response){
-            processEndpoint2(response.data)
+          $http.get(endpointB).then(function(endpoint2Data){
+            processEndpoint2(endpoint2Data.data)
           }, processEndpointError);
         }
 
@@ -23,26 +34,4 @@
         function processEndpointError(response) {
           console.log('error with an api request');
         }
-
-/*
-        var todoList = this;
-        todoList.todos = [
-          {text:'learn AngularJS', done:true},
-          {text:'build an AngularJS app', done:false}
-        ];
- 
-        todoList.addTodo = function() {
-          todoList.todos.push({text:todoList.todoText, done:false});
-          todoList.todoText = '';
-        };
- 
-        todoList.remaining = function() {
-          var count = 0;
-          angular.forEach(todoList.todos, function(todo) {
-            count += todo.done ? 0 : 1;
-          });
-          return count;
-        };
-*/
-
       });
